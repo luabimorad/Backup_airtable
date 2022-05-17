81a7423312f7f63e1da791329e9e5aeaae3783e4
LucianaAbimorad <95628762+luabimorad@users.noreply.github.com>
2022-05-17 18:07:32 +0200

    Add files via upload

---
+++ field.py
@@ -0,0 +1,39 @@
+d = {
+    "id": "recMUATc1pVhnvxpN",
+    "fields": {
+        "Id": 1,
+        "Name": "Luciana Abimorad\n",
+        "Last Modified": "2022-01-11",
+        "Attachments": [
+            {
+                "id": "atthmBDtyT0P8DLTp",
+                "width": 418,
+                "height": 627,
+                "url": "https://dl.airtable.com/.attachments/6e67849d4dd17bf76049bc6c306ff610/0f3b6ee9/Foto2.jpg",
+                "filename": "Foto2.jpg",
+                "size": 62714,
+                "type": "image/jpeg",
+                "thumbnails": {
+                    "small": {
+                        "url": "https://dl.airtable.com/.attachmentThumbnails/86ae183478bee8fba2738a82cc5327d5/ea2c6aca",
+                        "width": 24,
+                        "height": 36,
+                    },
+                    "large": {
+                        "url": "https://dl.airtable.com/.attachmentThumbnails/963cc51c0b0963871c038953ee6350ee/0d14dad6",
+                        "width": 418,
+                        "height": 627,
+                    },
+                    "full": {
+                        "url": "https://dl.airtable.com/.attachmentThumbnails/bf44c86cbe0115f43063aa14b8c92676/adba831d",
+                        "width": 3000,
+                        "height": 3000,
+                    },
+                },
+            }
+        ],
+        "Formula": 1,
+        "create_time": "2022-01-11",
+    },
+    "createdTime": "2022-01-11T18:08:29.000Z",
+}
